f638ddcbcf54119b9c8f40eaf3b3c59a056cf019
Dariell Vasquez <dariellv@yahoo.com>
2018-11-21 15:30:31 -0500

    Adding basic auth using HOC

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 // variables to be imported and used in our reducers switch statement, instead of hard coding a string
 export const SAVE_COMMENT = 'SAVE_COMMENT';
 export const FETCH_COMMENTS = 'FETCH_COMMENTS';
+export const CHANGE_AUTH = "change_auth";
 
 export function saveComment(comment){
     return {
@@ -17,5 +18,12 @@ export function fetchComments(){
     return {
         type: FETCH_COMMENTS,
         payload: request
+    }
+}
+
+export function changeAuth(isLoggedIn){
+    return {
+        type: CHANGE_AUTH,
+        payload: isLoggedIn
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import commentsReducer from './reducer_comments';
-
+import authReducer from "./auth";
 
 
 // link all reducers to a single root reducer, to create the redux store
 const rootReducer = combineReducers({
-  comments: commentsReducer
+  comments: commentsReducer,
+  auth: authReducer
 });
 
 export default rootReducer;
