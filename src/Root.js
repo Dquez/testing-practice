20801e0f941ca8320264b5d33c540330d3310e87
Dariell Vasquez <dariellv@yahoo.com>
2018-11-23 12:23:05 -0500

    Async replacement for redux-promise complete

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Provider} from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import ReduxPromise from "redux-promise";
+import async from "./middlewares/async";
 import reducers from "reducers";
 
 export default ({children, initialState = {}}) => {
     // const createStoreWithMiddleware = createStore(reducers, initialState, applyMiddleware(ReduxPromise))  store={createStoreWithMiddleware}
-    const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+    const createStoreWithMiddleware = applyMiddleware(async)(createStore);
     return(
      // data returned from reducers acts as redux store
     <Provider store={createStoreWithMiddleware(reducers, initialState)}>
--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,8 +1,12 @@
 export default ({dispatch}) => next => action =>{
     // check to see if action has a promise on its 'payload' property
     // if it does, wait for it to resolve, otherwise, pass the action to the next middleware
+    debugger;
     if(!action.payload || !action.payload.then){
         return next(action);
     }
-
-}
+    action.payload.then((response)=>{
+        const newAction = {...action, payload: response};
+        dispatch(newAction);
+    })
+}   
